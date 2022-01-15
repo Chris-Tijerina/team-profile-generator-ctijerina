1856e58b42052582024248cf5b7c89d2efc7594b
Christopher <ctijerina@live.com>
2022-01-15 17:34:29 -0600

    Added manager constructor with extension of employee and tested the constructor

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -10,28 +10,28 @@ test("creates a new employee", () => {
 });
 
 test("test out the get name method", () => {
-    const employee = new Employee("Ted", 1 , "ted@mosbiusdesigns.com")
+    const employee = new Employee("Ted", 1, "ted@mosbiusdesigns.com")
     const name = employee.getName();
 
     expect(name).toBe("Ted");
 });
 
 test("test out the get id method", () => {
-    const employee = new Employee("Ted", 123456 , "ted@mosbiusdesigns.com")
+    const employee = new Employee("Ted", 123456, "ted@mosbiusdesigns.com")
     const id = employee.getId();
 
     expect(id).toEqual(expect.any(Number));
 });
 
 test("test out the get email method", () => {
-    const employee = new Employee("Ted", 123456 , "ted@mosbiusdesigns.com")
+    const employee = new Employee("Ted", 123456, "ted@mosbiusdesigns.com")
     const email = employee.getEmail();
 
     expect(email).toEqual(expect.stringContaining("@"));
 });
 
 test("test out the get role method", () => {
-    const employee = new Employee("Ted", 123456 , "ted@mosbiusdesigns.com")
+    const employee = new Employee("Ted", 123456, "ted@mosbiusdesigns.com")
     const role = employee.getRole();
 
     expect(role).toEqual("Employee");
--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,9 +1,39 @@
-// const Manager = require("../lib/Manager")
+const Manager = require("../lib/Manager")
 
-// test("creates a new employee", () => {
-//     const employee = new Employee("Ted")
+test("creates a new manager", () => {
+    const manager = new Manager("Ted", 123456, "ted@mosbiusdesigns.com", "A113");
 
-//     expect(employee.name).toBe("Ted");
-//     expect(employee.id).toEqual(expect.any(Number));
-//     expect(employee.email).stringContaining("@");
-// })
+
+    expect(manager.name).toBe("Ted");
+    expect(manager.id).toEqual(expect.any(Number));
+    expect(manager.email).toEqual(expect.stringContaining("@"));
+    expect(manager.officeNumber).toEqual(expect.anything());
+});
+
+// test("test out the get name method", () => {
+//     const manager = new Manager("Ted", 1, "ted@mosbiusdesigns.com")
+//     const name = manager.getName();
+
+//     expect(name).toBe("Ted");
+// });
+
+// test("test out the get id method", () => {
+//     const manager = new Manager("Ted", 123456, "ted@mosbiusdesigns.com")
+//     const id = manager.getId();
+
+//     expect(id).toEqual(expect.any(Number));
+// });
+
+// test("test out the get email method", () => {
+//     const manager = new Manager("Ted", 123456, "ted@mosbiusdesigns.com")
+//     const email = manager.getEmail();
+
+//     expect(email).toEqual(expect.stringContaining("@"));
+// });
+
+// test("test out the get role method", () => {
+//     const manager = new Manager("Ted", 123456, "ted@mosbiusdesigns.com")
+//     const role = manager.getRole();
+
+//     expect(role).toEqual("Manager");
+// });
