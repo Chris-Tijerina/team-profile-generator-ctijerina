5db60c3bdfe52aec319c6222c51804435ce6171e
Christopher <ctijerina@live.com>
2022-01-15 23:50:15 -0600

    added Team class, as well as initializing function with questions for the manager object

--- index.js
+++ index.js
@@ -1,11 +1,64 @@
-const inquirer = require ("inquirer");
+const inquirer = require("inquirer");
 const fs = require("fs");
+const Manager = require("./lib/Manager");
+const Intern = require("./lib/Intern");
+const Engineer = require("./lib/Engineer");
+const generatePage = require('./src/page-template');
 
-const questions = () => {
-    return inquirer.prompt({
-        type: "text", 
-        name: "name", 
-        message: "What is the name of this employee?"
-    })
+class Team {
+    constructor() {
+        this.manager;
+    }
 
+    initializeTeam() {
+        inquirer.prompt([
+            {
+                type: "text",
+                name: "name",
+                message: "What is the name of the manager?"
+            },
+            {
+                type: "text",
+                name: "id",
+                message: "What is the manager's id number?"
+            },
+            {
+                type: "text",
+                name: "email",
+                message: "What is the manager's email address?"
+            },
+            {
+                type: "text",
+                name: "officeNumber",
+                message: "What is the manager's office number?"
+            }
+        ])
+            .then(data  => {
+                this.manager = new Manager(data.name, data.id, data.email, data.officeNumber);
+                console.log(this.manager);
+                this.checkMoreMembers();
+            })
+    };
+
+    checkMoreMembers() {
+        inquirer.prompt([ 
+            {
+                type: "confirm",
+                name: "moreMembers",
+                message: "Would you like to add more members of the team?",
+                default: false
+            }
+        ])
+        .then(data => {
+            if(data.moreMembers) {
+                console.log("add more members")
+            } else {
+                console.log("no more memebers")
+            }
+        })
+    }
 }
+
+new Team().initializeTeam();
+
+
